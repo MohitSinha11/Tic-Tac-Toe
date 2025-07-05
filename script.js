cbf3eaa1d90f426aaa4bc79ba4292c4ab64b471e
MohitSinha11 <sinhamohit1122@gmail.com>
2025-07-06 02:24:06 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,94 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPosition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function initGame() {
+  currentPlayer = "X";
+  gameGrid = ["", "", "", "", "", "", "", "", ""];
+  boxes.forEach((box, index) => {
+    box.innerText = "";
+    boxes[index].style.pointerEvents = "all";
+    box.classList.remove("win");
+  });
+  newGameBtn.classList.remove("active");
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+function swapPlayers() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+function checkGameOver() {
+  let answer = "";
+
+  winningPosition.forEach((position) => {
+    if (
+      (gameGrid[position[0]] !== "" ||
+        gameGrid[position[1]] !== "" ||
+        gameGrid[position[2]] !== "") &&
+      gameGrid[position[0]] === gameGrid[position[1]] &&
+      gameGrid[position[1]] === gameGrid[position[2]]
+    ) {
+      if (gameGrid[position[0]] === "X") answer = "X";
+      else answer = "Y";
+      boxes.forEach((box) => {
+        box.style.pointerEvents = "none";
+      });
+      boxes[position[0]].classList.add("win");
+      boxes[position[1]].classList.add("win");
+      boxes[position[2]].classList.add("win");
+      gameInfo.innerText = `Winner 🏆 - ${answer}`;
+    }
+  });
+
+  if (answer !== "") {
+    newGameBtn.classList.add("active");
+    return;
+  }
+
+  let fillCount = 0;
+
+  gameGrid.forEach((box) => {
+    if (box !== "") fillCount++;
+  });
+
+  if (fillCount === 9) {
+    gameInfo.innerText = `Game Tied !!`;
+    newGameBtn.classList.add("active");
+  }
+}
+
+function handleClick(index) {
+  if (gameGrid[index] === "") {
+    boxes[index].innerHTML = currentPlayer;
+    gameGrid[index] = currentPlayer;
+    boxes[index].style.pointerEvents = "none";
+    swapPlayers();
+    checkGameOver();
+  }
+}
+
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    handleClick(index);
+  });
+});
+
+newGameBtn.addEventListener("click", initGame);
